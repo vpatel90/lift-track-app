5dd0266f3935a1e60e4237daa2e435f8a5a007e3
Vivek Patel <vnp229@gmail.com>
2020-02-04 12:29:56 -0600

    Staging API URL

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -4,8 +4,8 @@ import { navigate } from '../navigationRef';
 import { showMessage, hideMessage } from "react-native-flash-message";
 import Colors from '../constants/Colors';
 
-// const API_URL = 'https://secret-lowlands-64580.herokuapp.com';
-const API_URL = 'https://0432c64e.ngrok.io'
+const API_URL = 'https://secret-lowlands-64580.herokuapp.com';
+// const API_URL = 'https://0432c64e.ngrok.io'
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
