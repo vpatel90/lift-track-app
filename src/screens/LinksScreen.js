e3246ac75d0d90332aac824d6389b7675a243247
Todd Vrba <todd.vrba@gmail.com>
2020-02-05 20:52:27 -0600

    Charting Library

--- src/screens/LinksScreen.js
+++ src/screens/LinksScreen.js
@@ -1,10 +1,53 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet, Text, Dimensions } from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
 
 export default function LinksScreen() {
   return (
     <ScrollView style={styles.container}>
-      <Text style={{textAlign:"center"}}>Coming Soon!</Text>
+      <Text>Bezier Line Chart</Text>
+      <LineChart
+        data={{
+          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100
+              ]
+            }
+          ]
+        }}
+        width={Dimensions.get('window').width} // from react-native
+        height={220}
+        yAxisLabel='$'
+        yAxisSuffix='k'
+        chartConfig={{
+          backgroundColor: '#e26a00',
+          backgroundGradientFrom: '#fb8c00',
+          backgroundGradientTo: '#ffa726',
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          },
+          propsForDots: {
+            r: '6',
+            strokeWidth: '2',
+            stroke: '#ffa726'
+          }
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
     </ScrollView>
   );
 }
